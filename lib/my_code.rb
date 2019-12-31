efddff9684f1867513ff7f651031b62a098e6be8
K e n V <kenneth_valdivia@yahoo.com>
2019-12-31 00:07:59 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # Your Code Here
+
+[1, 2, 3].map_to_square { |n| n * 2 } # => [2, 4, 6]
+
+
+def famous_movie_actors(element1, element2)
+  yield("Actors #{element1} and #{element2} are")
+end
+ 
+famous_movie_actors("Robert De Niro", "Al Pacino") { |innards| "#{innards} are legends" }
+#=> "Actors Robert De Niro and Al Pacino are legends"
