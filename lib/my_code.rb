eebad4a19c427ff322f8949e2be1271c614ea06f
K e n V <kenneth_valdivia@yahoo.com>
2019-12-31 00:04:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,3 +2,10 @@
 
 [1, 2, 3].map { |n| n * 2 } # => [2, 4, 6]
 
+
+def famous_movie_actors(element1, element2)
+  yield("Actors #{element1} and #{element2} are")
+end
+ 
+famous_movie_actors("Robert Di Nero", "Al Pacino") { |innards| "#{innards} are great" }
+#=> "Actors Robert Di"
